c988461ee0e826a01700c1c00a574e9d98469c1f
Anselme <anselme@chorein.fr>
2016-11-13 19:16:06 +0100

    Gestion de la tilemap (niveau) et de l'interaction avec celle-ci

--- src/states/game.js
+++ src/states/game.js
@@ -5,28 +5,51 @@ class Game extends Phaser.State {
 
   constructor() {
     super();
+    this.jumpTimer = 0;
   }
   
   create() {
     // commons
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.game.time.desiredFps = 30;
     this.game.physics.arcade.gravity.y = 250;
+    this.game.time.desiredFps = 30;
 
-    this.cursors = this.game.input.keyboard.createCursorKeys();
-    this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     // Sprites
     this.background = this.game.add.sprite(0,0,'background');
     this.background.height = this.game.world.height;
     this.background.width = this.game.world.width;
 
-    var tilesize=128;
-    this.tilesprite = this.game.add.tileSprite(
-        0, this.game.world.bottom-tilesize, // x y
-        this.game.world.width*2, this.game.world.bottom-tilesize, // width height
-        'world', 1); // sprite + frame
-    this.tilesprite.scale.setTo(0.5, 0.5);
+    // tilemap
+    this.map = this.game.add.tilemap('tilemap');
+    this.map.addTilesetImage('world-spritesheet', 'world');
+
+    this.layer = this.map.createLayer('back');
+    //this.layer.scale.set(0.2);
+    this.layer.resizeWorld();
+    //this.layer.wrap = true;
+    this.layer.debug = true;
+    /*for(var y = 0; y < this.layer.map.height; ++y){
+      for(var x = 0; x < this.layer.map.width; ++x){
+        var tile = this.layer.map.getTile(x, y);
+        console.log(tile);
+      }
+    }*/
+
+    this.blocsLayer = this.map.createLayer('blocs');
+    //this.layer.scale.set(0.2);
+    this.blocsLayer.resizeWorld();
+    //this.blocsLayer.wrap = true;
+    //this.layer.debug = true;
+
+    this.map.setLayer(this.blocsLayer);
+    this.map.setCollisionBetween(1, 112);
+    // jumper
+    this.map.setTileIndexCallback(242, function(sprite, tile) { sprite.body.velocity.y = -500; }, this, this.layer);
+    // pics
+    this.map.setTileIndexCallback(250, function(sprite, tile) { sprite.body.velocity.y = -500; }, this, this.layer);
+    // water
+    this.map.setTileIndexCallback(130, function(sprite, tile) { sprite.body.velocity.y = -500; }, this, this.layer);
 
     this.player = this.game.add.sprite(32, 32, 'dude');
     this.player.animations.add('left', [0, 1, 2, 3], 10, true);
@@ -34,26 +57,11 @@ class Game extends Phaser.State {
     this.player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     // Physics
-    this.game.physics.enable([this.player, this.tilesprite], Phaser.Physics.ARCADE);
+    this.game.physics.enable([this.player], Phaser.Physics.ARCADE);
     this.player.body.bounce.y = 0.2;
     this.player.body.collideWorldBounds = true;
     this.player.body.setSize(20, 32, 5, 16);
 
-    //this.tilesprite.body.collideWorldBounds = true;
-    this.tilesprite.body.immovable = true;
-    this.tilesprite.body.allowGravity = false;
-    this.tilesprite.body.setSize(this.game.world.width*2, tilesize, 0, tilesize);
-
-
-    //setup UI
-    /*
-    this.countdownText = this.add.text(this.game.world.centerX, 0, '', {
-      font: '40px Arial', fill: '#ffffff', align: 'center'
-    });
-    this.countdownText.anchor.set(0.5,0);*/
-
-    //set up click listeners
-    //this.game.input.onDown.add(this.shoot, this);
 
     //setup audio
     this.music = this.game.add.audio('musicGame');
@@ -65,19 +73,19 @@ class Game extends Phaser.State {
     //this.game.add.existing(this.crosshairs);
     //this.game.add.existing(this.target);
 
-    //this.tilesprite.visible=true;
-
     //setup a timer to end the game
     //this.endGameTimer = this.game.time.create();
     //this.endGameTimer.add(Phaser.Timer.SECOND * 15, this.endGame,this);
     //this.endGameTimer.start();
+
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   }
 
 
   update() {
-    this.game.physics.arcade.collide(this.player, this.tilesprite);
-    //this.game.debug.body(this.tilesprite);
-
+    this.game.physics.arcade.collide(this.player, this.blocsLayer);
+    this.game.physics.arcade.collide(this.player, this.layer);
 
     this.player.body.velocity.x = 0;
 
@@ -120,10 +128,10 @@ class Game extends Phaser.State {
       }
     }
 
-    if (this.jumpButton.isDown || this.cursors.up.isDown /*&& this.player.body.onFloor()*/ && this.game.time.now > this.jumpTimer)
-    {
+    if ((this.jumpButton.isDown || this.cursors.up.isDown) && this.game.time.now > this.jumpTimer) {
+      //this.player.body.onFloor()
       this.player.body.velocity.y = -250;
-      this.jumpTimer = this.game.time.now + 750;
+      this.jumpTimer = this.game.time.now + 1500;
     }
   }
 
@@ -132,6 +140,16 @@ class Game extends Phaser.State {
     this.game.state.start('gameover');
   }
 
+  changeMapTileSetArray(tileSetKey){
+    this.map.tiles = [];
+    var tileSet = Constant.TILE_SET_DICT[tileSetKey];
+    var currentTileSetId = this.map.getTilesetIndex(tileSetKey);
+    for(var i = 0;i<tileSet.tileList.length;i++){
+      this.map.tiles.push([tileSet.tileList[i].x,tileSet.tileList[i].y,currentTileSetId]);
+    }
+  };
 }
+
+
 
 export default Game;
